f55a04ad5d5831edb6a83852d846d000286d90b1
Jay Palat <palatv@Jay-Palats-MacBook-Pro.local>
2012-04-06 13:57:35 -0400

    Problem1.rb

--- ProjectEuler/Ruby/tests/Problem1Test.rb
+++ ProjectEuler/Ruby/tests/Problem1Test.rb
@@ -12,7 +12,17 @@ class Problem1Test < Test::Unit::TestCase
     end
 
     def test_factoring
-        assert_equals 1, @myproblem.factor(1);
-        assert true
-    end
+        assert_equal [1], @myproblem.factor(1)
+		assert_equal [1,2], @myproblem.factor(2)
+		assert_equal [1,2,5,10], @myproblem.factor(10)
+	end
+
+	def test_factored_by_3
+		assert_equal false, @myproblem.isAFactor?(3,1)
+		assert_equal true, @myproblem.isAFactor?(3,3)
+	end
+
+	def test_sum_of_multiples
+		assert_equal 23, @myproblem.sumOfMultiples(10);
+	end
 end
